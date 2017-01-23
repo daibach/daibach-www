e634531769271b3a661f5bd0f7ef69c0ce171027
Dafydd Vaughan <dai@daibach.co.uk>
2017-01-23 14:23:27 +0000

    Updated homepage to match new template

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,4 +1,17 @@
-<p class="introduction">I make stuff for the web at the Government Digital Service.</p>
-<p class="introduction">I'm a Technical Architect based in London, building GOV.UK - the single website for government. Previously I've helped build new digital services at the DVLA and Consumer Focus.</p>
-
-<p><a href="<?php echo site_url('profile'); ?>" title="Read my profile">Read more about me</a> or <a href="<?php echo site_url('contact'); ?>" title="Send me a message">get in touch</a>.</p>
+<div class="container">
+  <div class="row">
+    <div class="col-md-2 col-sm-2 col-xs-12">
+      <img src="/images/dai-2016.jpg" id="dai-logo" class="img-rounded img-responsive" width="140"/>
+    </div>
+    <div class="col-md-10 col-sm-10 col-xs-12">
+      <header class="masthead">
+        <h1>Hello, I'm <span>Dafydd Vaughan</span></h1>
+        <h2>I help people make things for the web</h2>
+      </header>
+      <p class="introduction">I'm a technical architect, product manager and digital strategist.</p>
+      <p class="introduction">I build teams, develop products and transform organisations.</p>
+      <p class="introduction">I'm the director of <a href="https://www.cedyrn.com">Cedyrn Ltd</a>. I was a founder of <a href="https://gds.blog.gov.uk"><abbr title="Government Digital Service">GDS</abbr></a>. I've built things for the DVLA, Consumer Focus and Thomas Cook.</p>
+      <p><a href="<?php echo site_url('profile'); ?>" title="Read my profile">Read more about me</a> or <a href="<?php echo site_url('contact'); ?>" title="Send me a message">get in touch</a>.</p>
+    </div>
+  </div>
+</div>
