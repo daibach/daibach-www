e7673282ed271a0d869772d520d2105514eb8511
Dafydd Vaughan <dai@daibach.co.uk>
2017-01-30 11:47:56 +0000

    Remove unused pages from menu bar

--- application/views/templates/sidebars/about.php
+++ application/views/templates/sidebars/about.php
@@ -1,8 +1,6 @@
 <div class="sidebar">
   <ul class="nav nav-pills nav-stacked">
     <li<?php if(isset($site_page) && $site_page=='profile') :?> class="active"<?php endif; ?>><a href="/profile">My profile</a></li>
-    <li<?php if(isset($site_page) && $site_page=='more-me') :?> class="active"<?php endif; ?>><a href="/more">Find me online</a></li>
-    <li<?php if(isset($site_page) && $site_page=='writings') :?> class="active"<?php endif; ?>><a href="/blogs">Things I've written</a></li>
     <li<?php if(isset($site_page) && $site_page=='contact') :?> class="active"<?php endif; ?>><a href="/contact">Contact me</a></li>
   </ul>
 </div><!-- /.sidebar -->
