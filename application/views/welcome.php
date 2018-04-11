3b3f59b1769347a89ded7f4c9bfe57bac03d65cf
Dafydd Vaughan <dai@daibach.co.uk>
2018-04-11 21:18:38 +0100

    Update homepage design

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,17 +1,24 @@
+<header class="masthead">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-2">
+        <img src="/images/dai-2016.jpg" id="dai-logo" class="rounded" width="140"/>
+      </div>
+      <div class="col">
+          <h1>Hello, I'm <span>Dafydd Vaughan</span></h1>
+          <h2>I help people make things for the web</h2>
+        </header>
+      </div>
+    </div>
+  </div>
+</header>
 <div class="container">
   <div class="row">
-    <div class="col-lg-2">
-      <img src="/images/dai-2016.jpg" id="dai-logo" class="rounded" width="140"/>
-    </div>
-    <div class="col">
-      <header class="masthead">
-        <h1>Hello, I'm <span>Dafydd Vaughan</span></h1>
-        <h2>I help people make things for the web</h2>
-      </header>
-      <p class="introduction">I'm a technical architect, product manager and digital strategist.</p>
-      <p class="introduction">I build teams, develop products and transform organisations.</p>
-      <p class="introduction">I'm the director of <a href="https://www.cedyrn.com">Cedyrn Ltd</a>. I was a founder of <a href="https://gds.blog.gov.uk"><abbr title="Government Digital Service">GDS</abbr></a>. I've built things for the DVLA, Consumer Focus and Thomas Cook.</p>
-      <p><a href="<?php echo site_url('profile'); ?>" title="Read my profile">Read more about me</a> or <a href="<?php echo site_url('contact'); ?>" title="Send me a message">get in touch</a>.</p>
+    <div class="col offset-lg-2">
+      <p class="lead">I'm a technical architect, product manager and digital strategist.</p>
+      <p class="lead">I build teams, develop products and transform organisations.</p>
+      <p class="lead">I'm the director of <a href="https://www.cedyrn.com">Cedyrn Ltd</a>. I was a founder of <a href="https://gds.blog.gov.uk"><abbr title="Government Digital Service">GDS</abbr></a>. I've built things for the DVLA, Consumer Focus and Thomas Cook.</p>
+      <p class="lead"><a href="<?php echo site_url('profile'); ?>" title="Read my profile">Read more about me</a> or <a href="<?php echo site_url('contact'); ?>" title="Send me a message">get in touch</a>.</p>
     </div>
   </div>
 </div>
