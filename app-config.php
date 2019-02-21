620b1a9e7b73cff85b0bc74cbd41edc54f6f9886
Dafydd Vaughan <dai@daibach.co.uk>
2019-02-21 11:08:44 +0000

    get config values from env variables

--- app-config.php
+++ app-config.php
@@ -1,21 +1,18 @@
 <?php
-define('SITE_NAME',             'Dafydd Vaughan');
-define('SITE_BASE_URL',         'https://dafyddvaughan.uk');
-define('SITE_LOG_THRESHOLD',    1);
-define('SITE_LOG_PATH',         '/data/vhost/production.dafyddvaughan.uk/shared/logs/ci/');
-define('SITE_CACHE_PATH',       './assets/cache/');
-define('SITE_ENCRYPT_KEY',      'xxx');
-define('SITE_COOKIE_NAME',      'daibach');
-define('SITE_COOKIE_DOMAIN',    '.dafyddvaughan.uk');
-define('SITE_COOKIE_PREFIX',    'daibach_');
+define('SITE_NAME',               'Dafydd Vaughan');
+define('SITE_BASE_URL',           getenv('APPSETTING_SITE_BASE_URL'));
+define('SITE_LOG_THRESHOLD',      0);
+define('SITE_ENCRYPT_KEY',        'xxx');
+define('SITE_COOKIE_NAME',        'daibach');
+define('SITE_COOKIE_DOMAIN',      '.dafyddvaughan.uk');
+define('SITE_COOKIE_PREFIX',      'daibach_');
 
+define('GA_CODE',                 getenv('APPSETTING_GA_CODE'));
 
-define('GA_CODE',               getenv('APPSETTING_GA_CODE'));
-
-DEFINE('CONTACT_API_CODE',        'xxx');
-DEFINE('CONTACT_API_KEY',         'xxx');
-DEFINE('CONTACT_API_ENDPOINT',    'xxx');
+DEFINE('CONTACT_API_CODE',        getenv('APPSETTING_CONTACT_API_USER'));
+DEFINE('CONTACT_API_KEY',         getenv('APPSETTING_CONTACT_API_KEY'));
+DEFINE('CONTACT_API_ENDPOINT',    getenv('APPSETTING_CONTACT_API_ENDPOINT'));
 DEFINE('CONTACT_API_UA',          'Contact Message Sender/0.1 (+https://dafyddvaughan.uk)');
 DEFINE('CONTACT_API_HTTP_AUTH',   TRUE);
-DEFINE('CONTACT_API_HTTP_AUTH_U', 'xxx');
-DEFINE('CONTACT_API_HTTP_AUTH_P', 'xxx');
+DEFINE('CONTACT_API_HTTP_AUTH_U', getenv('APPSETTING_CONTACT_API_HTTPAUTH_U'));
+DEFINE('CONTACT_API_HTTP_AUTH_P', getenv('APPSETTING_CONTACT_API_HTTPAUTH_P'));
