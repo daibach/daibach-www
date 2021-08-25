c3a8875e6ce52b3fef65a6dc6155e9cfdf30d519
Dafydd Vaughan <dafydd.vaughan@cedyrn.com>
2021-08-25 08:47:22 +0100

    Design tweaks to use new photo and colours

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,23 +1,15 @@
-<header class="masthead">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-2">
-        <img src="/images/dai-current.jpg" id="dai-logo" class="rounded" width="140"/>
-      </div>
-      <div class="col">
-          <h1>Hello, I'm <span>Dafydd Vaughan</span></h1>
-          <h2>I'm a digital technology specialist.</h2>
-        </header>
-      </div>
-    </div>
-  </div>
-</header>
-<div class="container">
+<div class="container" id="homepage-content">
   <div class="row">
-    <div class="col offset-lg-2">
-      <p class="lead">I advise organisations on digital transformation, replacement of legacy technology, and how to build strong digital teams.</p>
-      <p class="lead">I'm the director of <a href="https://www.cedyrn.com">Cedyrn Ltd</a> and a member of the advisory panel for the <a href="https://twitter.com/cdps_wales">Centre for Digital Public Services</a> in Wales. I was a co-founder of the UK <a href="https://gds.blog.gov.uk">Government Digital Service</a>.</p>
-      <p class="lead"><a href="<?php echo site_url('profile'); ?>" title="Read my profile">Read more about me</a> or <a href="<?php echo site_url('contact'); ?>" title="Send me a message">Get in touch</a>.</p>
+    <div class="col">
+        <h1>Hello...<br/><span>I'm Dafydd Vaughan</span></h1>
+        <h2>I'm a digital technology specialist.</h2>
+        <hr/>
+        <p class="lead">I advise organisations on digital transformation, replacement of legacy technology, and how to build strong digital teams.</p>
+        <p class="lead">I'm the director of <a href="https://www.cedyrn.com">Cedyrn Ltd</a> and a member of the advisory panel for the <a href="https://twitter.com/cdps_wales">Centre for Digital Public Services</a> in Wales. I was a co-founder of the UK <a href="https://gds.blog.gov.uk">Government Digital Service</a>.</p>
+        <p class="lead"><a href="<?php echo site_url('profile'); ?>" title="Read my profile">Read more about me</a> or <a href="<?php echo site_url('contact'); ?>" title="Send me a message">Get in touch</a>.</p>
+    </div>
+    <div class="col-lg-4">
+      <img src="/images/dai-2021d.jpg" id="dai-logo" class="rounded" width="280"/>
     </div>
   </div>
 </div>
