a0c9f024c907b8ac2d6d2dab78f3ff55228e65d0
Dafydd Vaughan <dafydd.vaughan@cedyrn.com>
2023-09-19 10:29:06 +0100

    update job titles

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -3,5 +3,5 @@
 <h2>I'm a digital technology specialist.</h2>
 <hr/>
 <p class="lead">I advise organisations on digital transformation, replacement of legacy technology, and how to build strong digital teams.</p>
-<p class="lead">I'm the director of <a href="https://www.cedyrn.com">Cedyrn Ltd</a> and a member of the advisory panel for the <a href="https://twitter.com/cdps_wales">Centre for Digital Public Services</a> in Wales. I was a co-founder of the UK <a href="https://gds.blog.gov.uk">Government Digital Service</a>.</p>
+<p class="lead">I run <a href="https://www.cedyrn.com">Cedyrn Ltd</a> and am a member of the advisory panel for the <a href="https://twitter.com/cdps_wales">Centre for Digital Public Services</a> in Wales. I was a co-founder of the UK <a href="https://gds.blog.gov.uk">Government Digital Service</a>.</p>
 <p class="lead"><a href="<?php echo site_url('profile'); ?>" title="Read my profile">Read more about me</a> or <a href="<?php echo site_url('contact'); ?>" title="Send me a message">Get in touch</a>.</p>
